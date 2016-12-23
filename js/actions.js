d332d79d6d478c7b72599b0d03b6b1c1308859a7
Rey David Dominguez Soto <rdominguez@tecnologer.net>
2016-12-23 23:29:08 +0300

    First commit

---
+++ js/actions.js
@@ -0,0 +1,61 @@
+(function(){
+	var inputMorse = document.getElementById("morseInput");
+	var ul = document.getElementById("ulCodigo");
+	var morseMessage=[];
+
+	function addCode(){
+		morseMessage.push(inputMorse.value);
+
+		var li=document.createElement("li");
+		li.innerHTML = "<span style='font-size: x-large;'>"+inputMorse.value+"</span>";
+		ul.appendChild(li);
+
+		inputMorse.value="";
+		inputMorse.focus();
+	}
+
+	function decodeMorse(){		
+		var message ="";
+		for(var x=0;x<morseMessage.length;x++){
+			var input = morseMessage[x].split("");
+
+			var morseAux = _MORSE_CODE_;
+			for(var i=0;i<input.length;i++){
+				if(input[i]==="."){
+					morseAux = morseAux && morseAux.left;
+				}
+				else if(input[i]==="-"){
+					morseAux = morseAux && morseAux.rigth;
+				}
+			}
+
+			message+=morseAux.data || "?";
+		}
+
+		// console.log(message);
+		document.getElementById("message").innerHTML = message;
+	}
+
+	function isValidCode(event){
+		var keyCode = event.keyCode || event.which;
+
+		if(keyCode !== 13 && keyCode !==8 && keyCode!==116 && event.key!=="-" &&  event.key!=="."){
+			event.preventDefault();
+		}
+		else if(keyCode === 13){
+			addCode();
+		}		
+	}
+
+	function clear(){
+		morseMessage=[];
+		inputMorse.value="";
+		ul.innerHTML = "";
+		document.getElementById("message").innerHTML = "";
+	}
+	inputMorse.addEventListener("keydown",isValidCode);
+	document.getElementById("btnTraducir").addEventListener("click",decodeMorse);
+	document.getElementById("btnLimpiar").addEventListener("click",clear);
+
+	inputMorse.focus();
+})();
---
+++ js/morse_code.js
@@ -0,0 +1,277 @@
+const _MORSE_CODE_ = {
+    "data": "",
+    "left": {
+        "data": "E",
+        "left": {
+            "data": "I",
+            "left": {
+                "data": "S",
+                "left": {
+                    "data": "H",
+                    "left": {
+                        "data": "5",
+                        "left": null,
+                        "rigth": null
+                    },
+                    "rigth": {
+                        "data": "4",
+                        "left": null,
+                        "rigth": null
+                    }
+                },
+                "rigth": {
+                    "data": "V",
+                    "left": {
+                        "data": "Ŝ",
+                        "left": null,
+                        "rigth": null
+                    },
+                    "rigth": {
+                        "data": "3",
+                        "left": null,
+                        "rigth": null
+                    }
+                }
+            },
+            "rigth": {
+                "data": "U",
+                "left": {
+                    "data": "F",
+                    "left": {
+                        "data": "É",
+                        "left": null,
+                        "rigth": null
+                    },
+                    "rigth": null
+                },
+                "rigth": {
+                    "data": "Ü",
+                    "left": {
+                        "data": "Đ",
+                        "left": {
+                            "data": "?",
+                            "left": null,
+                            "rigth": null
+                        },
+                        "rigth": {
+                            "data": "_",
+                            "left": null,
+                            "rigth": null
+                        }
+                    },
+                    "rigth": {
+                        "data": "2",
+                        "left": null,
+                        "rigth": null
+                    }
+                }
+            }
+        },
+        "rigth": {
+            "data": "A",
+            "left": {
+                "data": "R",
+                "left": {
+                    "data": "L",
+                    "left": null,
+                    "rigth": {
+                        "data": "È",
+                        "left": {
+                            "data": "\"",
+                            "left": null,
+                            "rigth": null
+                        },
+                        "rigth": null
+                    }
+                },
+                "rigth": {
+                    "data": "Ä",
+                    "left": {
+                        "data": "+",
+                        "left": null,
+                        "rigth": {
+                            "data": ".",
+                            "left": null,
+                            "rigth": null
+                        }
+                    },
+                    "rigth": null
+                }
+            },
+            "rigth": {
+                "data": "W",
+                "left": {
+                    "data": "P",
+                    "left": {
+                        "data": "Þ",
+                        "left": null,
+                        "rigth": null
+                    },
+                    "rigth": {
+                        "data": "À",
+                        "left": {
+                            "data": "@",
+                            "left": null,
+                            "rigth": null
+                        },
+                        "rigth": null
+                    }
+                },
+                "rigth": {
+                    "data": "J",
+                    "left": {
+                        "data": "Ĵ",
+                        "left": null,
+                        "rigth": null
+                    },
+                    "rigth": {
+                        "data": "1",
+                        "left": {
+                            "data": "'",
+                            "left": null,
+                            "rigth": null
+                        },
+                        "rigth": null
+                    }
+                }
+            }
+        }
+    },
+    "rigth": {
+        "data": "T",
+        "left": {
+            "data": "N",
+            "left": {
+                "data": "D",
+                "left": {
+                    "data": "B",
+                    "left": {
+                        "data": "6",
+                        "left": null,
+                        "rigth": {
+                            "data": "-",
+                            "left": null,
+                            "rigth": null
+                        }
+                    },
+                    "rigth": {
+                        "data": "=",
+                        "left": null,
+                        "rigth": null
+                    }
+                },
+                "rigth": {
+                    "data": "X",
+                    "left": {
+                        "data": "/",
+                        "left": null,
+                        "rigth": null
+                    },
+                    "rigth": null
+                }
+            },
+            "rigth": {
+                "data": "K",
+                "left": {
+                    "data": "C",
+                    "left": {
+                        "data": "Ç",
+                        "left": null,
+                        "rigth": null
+                    },
+                    "rigth": {
+                        "data": "\u0020",
+                        "left": {
+                            "data": ";",
+                            "left": null,
+                            "rigth": null
+                        },
+                        "rigth": {
+                            "data": "!",
+                            "left": null,
+                            "rigth": null
+                        }
+                    }
+                },
+                "rigth": {
+                    "data": "Y",
+                    "left": {
+                        "data": "Ĥ",
+                        "left": null,
+                        "rigth": {
+                            "data": "()",
+                            "left": null,
+                            "rigth": null
+                        }
+                    },
+                    "rigth": null
+                }
+            }
+        },
+        "rigth": {
+            "data": "M",
+            "left": {
+                "data": "G",
+                "left": {
+                    "data": "Z",
+                    "left": {
+                        "data": "7",
+                        "left": null,
+                        "rigth": null
+                    },
+                    "rigth": {
+                        "data": "",
+                        "left": null,
+                        "rigth": {
+                            "data": ",",
+                            "left": null,
+                            "rigth": null
+                        }
+                    }
+                },
+                "rigth": {
+                    "data": "Q",
+                    "left": {
+                        "data": "Ĝ",
+                        "left": null,
+                        "rigth": null
+                    },
+                    "rigth": {
+                        "data": "Ñ",
+                        "left": null,
+                        "rigth": null
+                    }
+                }
+            },
+            "rigth": {
+                "data": "O",
+                "left": {
+                    "data": "Ö",
+                    "left": {
+                        "data": "8",
+                        "left": {
+                            "data": ":",
+                            "left": null,
+                            "rigth": null
+                        },
+                        "rigth": null
+                    },
+                    "rigth": null
+                },
+                "rigth": {
+                    "data": "CH",
+                    "left": {
+                        "data": "9",
+                        "left": null,
+                        "rigth": null
+                    },
+                    "rigth": {
+                        "data": "0",
+                        "left": null,
+                        "rigth": null
+                    }
+                }
+            }
+        }
+    }
+}
